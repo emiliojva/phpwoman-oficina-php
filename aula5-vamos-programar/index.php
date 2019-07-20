3a71ab681f370b5a0a470547a1100302134ec377
Emilio Vieira <emiliojva@gmail.com>
2019-07-20 14:12:38 -0300

    aula 5 - vamos programar

---
+++ aula5-vamos-programar/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Exemplo</title>
+
+        <style>
+            
+            body { padding: 0px; margin:0px; }
+            
+            code {
+                padding: 10px;
+                display:block;
+                border: 1px dashed gray;
+                margin: 10px 5px ;
+                white-space: pre-line;
+                /* word-break: break-word; */
+                /* word-wrap: break-word; */
+                /* overflow-wrap: break-word; */
+                
+            }
+        </style>
+        
+    </head>
+    
+    <body>
+
+    <code>
+
+        <?PHP
+            /**
+             * Operadores Lógicos
+             * 
+             * São utilizados para realizar comparações lógicas retornando true ou false.
+             * 
+             */
+
+            $a = "a"; $b = "b";
+        ?>
+
+        <code>Vamos Programar</code>
+
+        <code>$a="a" e $b="b"
+            $a + $b
+            <?PHP
+            var_dump($a + $b); // verifica se os valores e os tipos sao iguais ?>
+        </code>
+
+
+
+
+
+    </body>
+
+</html>
