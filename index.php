757616df9a8fa808b8ed314bb63bc5bf370ef016
Emilio Vieira <emiliojva@gmail.com>
2019-07-20 11:51:20 -0300

    Hello World

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Exemplo</title>
+
+        <style>
+            
+            body { padding: 0px; margin:0px; }
+            
+            code {
+                padding: 10px;
+                display:block;
+                border: 1px dashed gray;
+                margin: 10px 5px ;
+            }
+        </style>
+        
+    </head>
+    
+    <body>
+
+    <code>
+        <?PHP 
+            echo "Olá, eu sou um script PHP!";
+        ?>
+    </code>
+
+    <code>
+        <?PHP
+            $cor = 'Amarelo';
+            echo $cor;
+        ?>
+    </code>
+
+    </body>
+
+</html>
